f56c6ac5e9ba900548ee08f05dc5e9c8ce59cad9
MaiCyclone <jyx940709@gmail.com>
2019-03-12 00:27:43 -0400

    Update programmer.py

--- programmer.py
+++ programmer.py
@@ -4,11 +4,24 @@ class Programmer():
 		self.name = name
 		self._age = age
 		self.__weight = weight
-		
+
 	def get_weight(self):
 		return self.__weight
-		
-		
+
+	def __eq__(self,other):
+		if isinstance(other,Programmer):
+			if(self._age == other._age):
+				print('same age')
+			else:
+				print('different age')
+		else:
+			print('We need you to input a programmer')
+#############################################################
+	def __getattribute__(self,name):						#
+		return super().__getattribute__(name);				#
+#############################################################
+
+
 class BackendProgrammer(Programmer):
 	def __init__(self,name,age,weight,language):
 		super().__init__(name,age,weight)
@@ -16,9 +29,11 @@ class BackendProgrammer(Programmer):
 
 if __name__ == '__main__':
 	jjj  = BackendProgrammer('jjj',24,120,'Python')
+	momo = Programmer('momo',25,100)
+	jjj == momo
 	print(dir(jjj))
 	print(jjj.__dict__)
 	print(jjj.get_weight())
 	print(type(jjj))
 	print( isinstance(jjj,Programmer))
-	
+	print(jjj._age)
